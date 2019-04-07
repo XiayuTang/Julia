8b3bd39b04cd1c995b3add50727fc66963c4fffb
XiayuTang <tangxg16@gmail.com>
2019-04-07 12:05:52 +0800

    matmul

--- matrix_computation.jl
+++ matrix_computation.jl
@@ -3,7 +3,7 @@
 """
     dot(x, y) -> c
 
-(Dot Product) If `x, y ∈ ℝⁿ`, then this algorithm computes their dot
+Algorithm 1.1.1 (Dot Product) If `x, y ∈ ℝⁿ`, then this algorithm computes their dot
 product `c = xᵀy`.
 """
 function dot(x::AbstractVector{T}, y::AbstractVector) where T <: Signed
@@ -28,7 +28,7 @@ end
 """
     saxpy!(x, y, a) -> y
 
-(Saxpy) If `x`, `y` ∈ ℝⁿ and `a` ∈ ℝ, then this algorithm overwrites
+Algorithm 1.1.2 (Saxpy) If `x`, `y` ∈ ℝⁿ and `a` ∈ ℝ, then this algorithm overwrites
 `y` with ``y + ax``.
 """
 function saxpy!(x::AbstractVector, y::AbstractVector, a::Real)
@@ -42,14 +42,44 @@ end
 """
     rogaxpy!(A, x, y) -> y
 
-(Row-Oriented Gaxpy) If `A` ∈ ``ℝ^{m×n}``, `x` ∈ ℝⁿ, and `y` ∈ ℝᵐ, then
-this algorithm overwrites `y` with ``Ax + y``.
+Algorithm 1.1.3 (Row-Oriented Gaxpy) If `A` ∈ ``ℝ^{m×n}``, `x` ∈ ℝⁿ, and `y` ∈ ℝᵐ, then this algorithm overwrites `y` with ``Ax + y``.
 """
 function rogaxpy!(A::AbstractMatrix, x::AbstractVector, y::AbstractVector)
     n = length(x)
     m = length(y)
     @assert (m, n) == size(A)
+    @inbounds for i in 1:n, j in 1:m
+        y[i] += A[i, j] * x[j]
+    end
+end
+
+"""
+    cogaxpy!(A, x, y) -> y
+
+Algorithm 1.1.4 (Column-Oriented Gaxpy) If `A` ∈ ``ℝ^{m×n}``, `x` ∈ ℝⁿ, and
+`y` ∈ ℝᵐ, then this algorithm overwrites `y` with  ``Ax+y``.
+"""
+function cogaxpy!(A::AbstractMatrix, x::AbstractVector, y::AbstractVector)
+    n = length(x)
+    m = length(y)
+    @assert (m, n) == size(A)
     @inbounds for j in 1:n, i in 1:m
         y[i] += A[i, j] * x[j]
+    end
+end
+
+"""
+    matmul!(A, B, C) -> C
+
+Algorithm 1.1.5 (ijk Matrix Multiplication) If `A` ∈ ``ℝ^{m×r}``, `B` ∈ ``ℝ^{r×n}``, and `C` ∈ ``ℝ^{m×n}`` are give, and this algorithm overwrites `C`
+with ``C + AB``.
+"""
+function matmul!(A::AbstractMatrix, B::AbstractMatrix, C::AbstractMatrix)
+    m, r = size(A)
+    r_, n = size(B)
+    m_, n_ = size(C)
+    @assert r == r_ && m == m_ && n == n_
+    @inbounds for j in 1:n, k in 1:r, i in 1:m
+        C[i, j] += A[i, k] * B[k, j]
     end
 end
--- test.jl
+++ test.jl
@@ -1,10 +1,16 @@
 # push!(LOAD_PATH,".")
 #
 # using LinearEquationsSolve
-function fib(n)
-    a, b = 1, 1
-    for _ in 1:n-1
-        a, b = b, a+b
-    end
-    b
-end
+# A = [1 2; 3 4; 5 6]
+# @inbounds for i = 1:3, j = 1:2
+# println(A[i, j])
+# end
+# function g(A)
+#       m,n = size(A)
+#       @simd for i=1:m
+#             @inbounds println(A[i,:])
+#       end
+# end
+# # a0 = ones(Int,19)
+# # b0 = ones(Int,19)
+# g(A)
