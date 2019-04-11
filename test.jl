2dcff7a11ae156597a0e99a80ae47d996958fe0a
XiayuTang <tangxg16@gmail.com>
2019-04-11 13:21:58 +0800

    init

--- test.jl
+++ test.jl
@@ -14,3 +14,14 @@
 # # a0 = ones(Int,19)
 # # b0 = ones(Int,19)
 # g(A)
+# using LinearAlgebra
+# @time begin
+# for _ in 1:100000
+#         s = 0.0
+#         x = rand(1000)
+#         y = rand(1000)
+#         s = dot(x,y)
+# end
+# end
+# using LinearAlgebra
+# Diagonal
