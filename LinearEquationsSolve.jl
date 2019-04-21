d45d2a51b8f0c83dd164a644d56761a41ab35bb1
XiayuTang <tangxg16@gmail.com>
2019-04-21 17:47:21 +0800

    Gauss_Seidel

--- LinearEquationsSolve.jl
+++ LinearEquationsSolve.jl
@@ -1,5 +1,5 @@
-# module LinearEquationsSolve
-# export diagchange!,jacobi
+module LinearEquationsSolve  # 模板
+export gauss_seidel
 using LinearAlgebra
 
 
@@ -11,7 +11,7 @@ using LinearAlgebra
 function isrowzero(A::AbstractMatrix)
     n = size(A,1)
     o = zeros(eltype(A),n)
-    for i ∈ 1:n
+    for i in 1:n
         if A[i,:] == o
             return true
         end
@@ -38,12 +38,12 @@ end
 
 
 """
-    diagchange!(A)
+    diagchange!(A::Matrix{T}) where T <: AbstractFloat
 
 若矩阵A的对角元的某几个为0，
 则通过初等行变换将0转换为非零元素
 """
-function diagchange!(A::AbstractMatrix{T}) where T <: AbstractFloat
+function diagchange!(A::Matrix{T}) where T <: AbstractFloat
     m,n = size(A)
     @assert m == n  # 要求 A为方阵
     @assert !isrowzero(A)
@@ -77,33 +77,6 @@ Jacobi迭代法求解线性方程组 ``Ax = b``
 - `x::AbstractVector{AbstractFloat}`  方程组的近似解
 - `n`  迭代次数
 
-# Mathematical Theory
-将系数矩阵``A``分解为
-```math
-A = L + D + U
-```
-其中``L``是下三角矩阵，由``A``的对角线之下的元素组成，``D``是对角矩阵，由``A``的对角线元素组成，``U``是上三角矩阵，由``A``的对角线之上的元素组成。
-
-给定迭代初值`x⁰`，则第``k+1``次迭代可表示为
-```math
-Dx^{k+1} + (L+U)x^{k} = b
-```
-即
-```math
-x^{k+1} = -D^{-1}(L+U)x^k + D^{-1}b
-```
-令
-```math
- r^k = b - Ax^k
-```
-则
-```math
-b = r^k + Ax^k = r^k + (L+D+U)x^k
-```
-于是Jacobi迭代形式可写为
-```math
-x^{k+1} = x^k + D^{-1}r^k
-```
 # Example
 ```jldoctest
 >julia a = [2.0 1.0; 1.0 1.0];
@@ -118,26 +91,75 @@ x^{k+1} = x^k + D^{-1}r^k
 34
 ```
 """
-function jacobi(A::AbstractMatrix{T},b::AbstractVector{T},x⁰::AbstractVector{T},ϵ::AbstractFloat=1e-5,maxiter::Int=100) where T <: AbstractFloat
+function jacobi(A::Matrix{T},b::Vector{T},x⁰::Vector{T},ϵ::Float64=1e-5,maxiter::Int=100) where T <: AbstractFloat
     diagchange!(A)
     n = 0
     D = Diagonal(diag(A))
     Dinv = inv(D)
     while true
         r⁰ = b - A*x⁰
-        x¹ = x⁰ + Dinv*r⁰
+        x = x⁰ + Dinv*r⁰
         n += 1
-        if norm(x¹-x⁰) < ϵ
-            return x¹,n
+        if norm(x-x⁰) < ϵ
+            return x, n
         end
         if n > maxiter
             println("超过最大迭代次数！")
             return nothing
         end
-        x⁰ = x¹
+        x⁰ = x
     end
 end
 
+
+"""
+    gauss_seidel(A,b,x⁰,ϵ,maxiter) -> (x,n)
+
+Gauss-Seidel迭代法求解线性方程组 ``Ax = b``
+
+# Aguments
+- `A::AbstractMatrix{AbstractFloat}`  系数矩阵
+- `b::AbstractVector{AbstractFloat}`  常数项
+- `x⁰::AbstractVector{AbstractFloat}`  迭代初值
+- `ϵ::AbstractFloat=1e-5`  允许误差
+- `maxiter::Int=100`  最大迭代次数
+- `x::AbstractVector{AbstractFloat}`  方程组的近似解
+- `n`  迭代次数
+
+# Example
+```jldoctest
+>julia a = [2.0 1.0; 1.0 1.0];
+>julia b = [3.0; 2.0];
+>julia x⁰ = [0.7;0.7];
+>julia x,n = jacobi(a,b,x⁰);
+>julia x
+2-element Array{Float64,1}:
+ 1.0000045776367188
+ 0.9999954223632812
+>julia n
+16
+```
+"""
+
+function gauss_seidel(A::Matrix{T}, b::Vector{T}, x⁰::Vector{T}, ϵ::Float64=1e-5,maxiter::Int=100) where T <: AbstractFloat
+    diagchange!(A)
+    n = 0
+    L = tril(A)
+    Linv = inv(L)
+    while true
+        r⁰ = b - A*x⁰
+        x = x⁰ + Linv*r⁰
+        n += 1
+        if norm(x-x⁰) < ϵ
+            return x, n
+        end
+        if n > maxiter
+            println("超过最大迭代次数！")
+            return nothing
+        end
+        x⁰ = x
+    end
+end
 """
     cg(A::Matrix{T}, b::Vector{T}, x₀::Vector{T}, e::Float64=1e-5) where {T <: AbstractFloat} -> AbstractVector
 
@@ -181,4 +203,4 @@ function cg(A::Matrix{T}, b::Vector{T}, x₀::Vector{T}, e::Float64=1e-5) where 
     x₀
 end
 
-# end  # module
+end  # module
--- test.jl
+++ test.jl
@@ -1,46 +1,7 @@
 # push!(LOAD_PATH,".")
-#
 # using LinearEquationsSolve
-# A = [1 2; 3 4; 5 6]
-# @inbounds for i = 1:3, j = 1:2
-# println(A[i, j])
-# end
-# function g(A)
-#       m,n = size(A)
-#       @simd for i=1:m
-#             @inbounds println(A[i,:])
-#       end
-# end
-# # a0 = ones(Int,19)
-# # b0 = ones(Int,19)
-# g(A)
-# using LinearAlgebra
-# @time begin
-# for _ in 1:100000
-#         s = 0.0
-#         x = rand(1000)
-#         y = rand(1000)
-#         s = dot(x,y)
-# end
-# end
-# using LinearAlgebra
-# Diagonal
-# function test1()
-#     s = 0.0
-#     for i in 1:100
-#         s += 1/i
-#     end
-# end
-# @time test1()
-# using LinearAlgebra
-# diagind
-using LinearAlgebra
-function test()
-    for _ in 1:100
-        A = rand(3000, 2000)
-        B = rand(2000, 1000)
-        A * B
-    end
-    nothing
-end
-@time test() 
+
+# a = [2.0 1.0; 1.0 1.0];
+# b = [3.0; 2.0];
+# x⁰ = [0.7;0.7];
+# x,n = gauss_seidel(a,b,x⁰)
